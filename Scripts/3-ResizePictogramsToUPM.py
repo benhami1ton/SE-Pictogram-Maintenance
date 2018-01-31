ba71dd5d53329667996ec960c17b1516db078178
Ben Hamilton <ben@benhamiltoncreative.com>
2018-01-31 00:03:42 -0600

    more simplification

--- Scripts/3-ResizePictogramsToUPM.py
+++ Scripts/3-ResizePictogramsToUPM.py
@@ -13,25 +13,66 @@ os.chdir(os.path.expanduser("~/Downloads/SE-Pictogram-Maintenance-0.3/EPS/Import
 currentDir = os.getcwd()
 completedDir = "Completed"
 thisEPSnewPath = ""
+PUAglyphs = []
+xMax = []
+yMax = []
+sameMax = []
 
 for myFont in Glyphs.fonts:
     for myGlyph in myFont.glyphs:
         if myGlyph.category == "Private Use":
-        	PUAglyphs.append(str(myGlyph.name))
+        	PUAglyphs.append(myGlyph)
+    print "PUAglyphs collected."
 
-myLayers = Glyphs.font.selectedLayers # current layer
+for eachGlyph in PUAglyphs:
+	for layer in eachGlyph.layers:
+		if layer.bounds.size.width > layer.bounds.size.height:
+			xMax.append(eachGlyph)
+			# print (eachGlyph.name + " is wide.")
+		if layer.bounds.size.width < layer.bounds.size.height:
+			yMax.append(eachGlyph)
+			# print (eachGlyph.name + " is tall.")
+		if layer.bounds.size.width == layer.bounds.size.height:
+			sameMax.append(eachGlyph)
+			# print (eachGlyph.name + " is square.")
+		if eachGlyph.name in xMax or yMax or sameMax:
+			pass
+		else:
+			print (eachGlyph.name + "-----ERROR-----")
 
-def scaleToUPM():
-	for thisLayer in myLayers:
-		print (thisLayer.parent.name + ' scaled by factor of ' + str(thisLayer.width / thisLayer.bounds.size.width))
-		thisLayer.applyTransform([
-                        		thisLayer.width / thisLayer.bounds.size.width, # x scale factor
-                        		0.0, # x skew factor
-                        		0.0, # y skew factor
-                        		thisLayer.width / thisLayer.bounds.size.width, # y scale factor
-                        		0.0, # x position
-                        		0.0  # y position
-                        	])
+def scaleToWidth():
+	# These two lists can be combined to simplify the loop.
+	widthPriority = xMax + sameMax
+
+	for glyph in widthPriority:
+		for layer in glyph.layers:
+			print (layer.parent.name + " | " + layer.name + ' scaled by factor of ' + str((1024 * 0.75) / layer.bounds.size.width))
+			xOrigin = layer.bounds.origin.x
+			yOrigin = layer.bounds.origin.y
+			layer.applyTransform([
+                        			(1024 * 0.75) / layer.bounds.size.width, # x scale factor
+                        			0.0, # x skew factor
+                        			0.0, # y skew factor
+                        			(1024 * 0.75) / layer.bounds.size.width, # y scale factor
+                        			0, # x position
+                        			0  # y position
+                        		])
+def scaleToHeight():
+	for glyph in yMax:
+		for layer in glyph.layers:
+			print (layer.parent.name + " | " + layer.name + ' scaled by factor of ' + str((1024 * 0.75) / layer.bounds.size.height))
+			xOrigin = layer.bounds.origin.x
+			yOrigin = layer.bounds.origin.y
+			layer.applyTransform([
+                        			(1024 * 0.75) / layer.bounds.size.height, # x scale factor
+                        			0.0, # x skew factor
+                        			0.0, # y skew factor
+                        			(1024 * 0.75) / layer.bounds.size.height, # y scale factor
+                        			0, # x position
+                        			0  # y position
+
+                        		])
+
 
 # Creates new directory for the selected glyph, so Glyphs.app can find and grab it
 def moveToCompletedDir():
@@ -40,16 +81,16 @@ def moveToCompletedDir():
 	if not os.path.exists(completedDir):
 		os.makedirs(completedDir)
 	shutil.move(thisImportEPSoldPath, thisImportEPSnewPath)
-	print "%s Moved into Completed directory." % thisLayer.parent.name
+	print "%s moved into Completed directory." % thisGlyph.name
 
 
 # Loop through selected glyphs, to check if there's a new glyph. If there is, it deletes
 # the paths of the selected glyph, imports the new ones, resizes them to the UPM.
-for thisLayer in myLayers:
-    thisEPSfile = "%s.eps" % (thisLayer.parent.name)
+for thisGlyph in PUAglyphs:
+    thisEPSfile = "%s.eps" % (thisGlyph.name)
     if os.path.exists(thisEPSfile):
     	moveToCompletedDir()
-    	scaleToUPM()
-#     	print (thisLayer.parent.name + ' was updated. \n')
-    else:
-    	print (thisLayer.parent.name + ' was not affected. \n')
+    	print (thisGlyph.name + ' was updated. \n')
+
+scaleToWidth()
+scaleToHeight()
--- Scripts/4-CreateOpenTypeLigaFeature.py
+++ Scripts/4-CreateOpenTypeLigaFeature.py
@@ -9,7 +9,7 @@ import GlyphsApp
 
 separator = "\n"
 PUAglyphs = []
-illegals = {" 0 ":" zero ", " 1 ":" one ", " 2 ":" two ", " 3 ":" three ", " 4 ":" four ", " 5 ":" five ", " 6 ":" six ", " 7 ":" seven ", " 8 ":" eight ", " 9 ":" nine ", " - ":" hyphen ", " _ ": " underscore "}
+illegals = {" 0 ":" zero ", " 1 ":" one ", " 2 ":" two ", " 3 ":" three ", " 4 ":" four ", " 5 ":" five ", " 6 ":" six ", " 7 ":" seven ", " 8 ":" eight ", " 9 ":" nine ", " - ":" hyphen ", " _ ": " underscore ", " . ":" period "}
 
 for myFont in Glyphs.fonts:
     for myGlyph in myFont.glyphs:
